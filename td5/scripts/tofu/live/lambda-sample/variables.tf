f3b5d052d0241695b535b9b543f08b920e0fe415
Parysnm <parys.noyon@gmail.com>
2025-01-30 15:37:12 +0100

    Ajout des variables manquantes au module Lambda

--- td5/scripts/tofu/live/lambda-sample/variables.tf
+++ td5/scripts/tofu/live/lambda-sample/variables.tf
@@ -1,34 +1,5 @@
 variable "name" {
-  description = "Nom de la fonction Lambda"
+  description = "The base name for the function and all other resources"
   type        = string
-}
-
-variable "src_dir" {
-  description = "Chemin du dossier source"
-  type        = string
-}
-
-variable "runtime" {
-  description = "Runtime de la fonction Lambda"
-  type        = string
-}
-
-variable "handler" {
-  description = "Handler de la fonction Lambda"
-  type        = string
-}
-
-variable "memory_size" {
-  description = "Taille mémoire de la Lambda"
-  type        = number
-}
-
-variable "timeout" {
-  description = "Délai d’exécution"
-  type        = number
-}
-
-variable "environment_variables" {
-  description = "Variables d'environnement"
-  type        = map(string)
+  default     = "lambda-sample"
 }
